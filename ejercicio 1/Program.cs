68ad3bba0b60b11a54812ee85d8f8ff0cf0d9b2a
cristobal <cristobal@DESKTOP-C6EVRJF>
2025-08-25 16:20:45 -0600

    Agregar archivos de proyecto.

---
+++ ejercicio 1/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Dame un número:");
+int numero = int.Parse(Console.ReadLine());
+
+bool esPrimo = true;
+var noPrrimo = false;
+if (numero <= 2)
+{
+    esPrimo = false;
+}
+else
+{
+    for (int i = 2; i < numero; i++)
+    {
+        if (numero % i == 0) // si hay un divisor
+        {
+            esPrimo = false;
+            break;
+        }
+    }
+}
+
+if (esPrimo)
+{
+    Console.WriteLine("El número " + numero + " es primo.");
+}
+else
+{
+    Console.WriteLine("El número " + numero + " no es primo.");
+}
